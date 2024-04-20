a978151381816174b9433400aafa1763f00c2a3c
rjspyk5 <rjspyk5@gmail.com>
2024-04-21 02:45:23 +0600

    connect database

--- index.js
+++ index.js
@@ -1,12 +1,36 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 5000;
 
 app.use(cors());
+app.use(express.json());
+
+const uri =
+  "mongodb+srv://rjspyk5:789789@cluster0.omgilvs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+const run = async () => {
+  try {
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log("database connected");
+  } finally {
+    await client.close();
+  }
+};
+run().catch(console.dir);
+
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("hello world");
 });
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+app.listen(port, () => console.log("server running"));
